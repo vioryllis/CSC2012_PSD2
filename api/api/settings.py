7914a0591f5dba32016290f3895fa1dcb1741128
vioryllis <jedzvioletear@gmail.com>
2024-03-19 01:20:53 +0800

    added allow host on api side

--- api/api/settings.py
+++ api/api/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-6r8n58q@zxqzt1v5bl1nzte!%lie7gwmbw007)^&a-9fn(*1ni
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.133.159', 'localhost', '127.0.0.1']
 
 
 # Application definition
