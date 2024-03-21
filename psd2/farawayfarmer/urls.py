96c14cfdf172e235cacfd3497519695fe82bcfb5
vioryllis <jedzvioletear@gmail.com>
2024-03-21 15:43:06 +0800

    add url for call_water_plant

--- psd2/farawayfarmer/urls.py
+++ psd2/farawayfarmer/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("plants", views.plants, name="plants"),
     path("plants/<int:plant_id>", views.plant, name="plant"),
+
+    # for testings
+    path("plants/call_water_plant", views.call_water_plant, name="call_water_plant"),
 ]
