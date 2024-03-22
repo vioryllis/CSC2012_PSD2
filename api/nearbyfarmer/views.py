e6e14e94a9cecffe2cb1b089611c898dad3577f7
vioryllis <jedzvioletear@gmail.com>
2024-03-23 02:26:48 +0800

    commented out toggle_auto_system

--- api/nearbyfarmer/views.py
+++ api/nearbyfarmer/views.py
@@ -16,7 +16,7 @@ def receive_data(request):
             # Parse the incoming JSON data
             data = json.loads(request.body.decode('utf-8'))
             print("data from m5stick: ", data)
-            
+
             # Extract sensor data and plant_id from the incoming data
             water_level = data['water_level']
             nutrient_level = data['nutrient_level']
@@ -117,37 +117,37 @@ def water_plant_amt(request):
     else:
         return JsonResponse({"error": "Only POST and GET methods are accepted."}, status=405)
 
-@csrf_exempt
-def toggle_auto_system(request):
-    if request.method == "POST":
-        # TODO: 
-        on_or_off = request.data.get('on_or_off')
+# @csrf_exempt
+# def toggle_auto_system(request):
+#     if request.method == "POST":
+#         # TODO: 
+#         on_or_off = request.data.get('on_or_off')
         
-        if on_or_off:
-            # Data to send to M5Stick
-            data_to_m5stick = {"message": "Request to turn on auto watering system"}
+#         if on_or_off:
+#             # Data to send to M5Stick
+#             data_to_m5stick = {"message": "Request to turn on auto watering system"}
             
-            # URL of the M5Stick endpoint
-            m5stick_url = "http://m5stick_ip_address/api/receive_data"
+#             # URL of the M5Stick endpoint
+#             m5stick_url = "http://m5stick_ip_address/api/receive_data"
             
-            # Send data to M5Stick
-            response_to_m5stick = requests.post(m5stick_url, json=data_to_m5stick)
+#             # Send data to M5Stick
+#             response_to_m5stick = requests.post(m5stick_url, json=data_to_m5stick)
             
-            if response_to_m5stick.status_code == 200:
-                return JsonResponse({"status": "success", "message": "Data sent to M5Stick successfully"})
-            else:
-                return JsonResponse({"status": "error", "message": "Failed to send data to M5Stick"}, status=500)
-        else:
-             # Data to send to M5Stick
-            data_to_m5stick = {"message": "Request to turn off auto watering system"}
+#             if response_to_m5stick.status_code == 200:
+#                 return JsonResponse({"status": "success", "message": "Data sent to M5Stick successfully"})
+#             else:
+#                 return JsonResponse({"status": "error", "message": "Failed to send data to M5Stick"}, status=500)
+#         else:
+#              # Data to send to M5Stick
+#             data_to_m5stick = {"message": "Request to turn off auto watering system"}
             
-            # URL of the M5Stick endpoint
-            m5stick_url = "http://m5stick_ip_address/api/receive_data"
+#             # URL of the M5Stick endpoint
+#             m5stick_url = "http://m5stick_ip_address/api/receive_data"
             
-            # Send data to M5Stick
-            response_to_m5stick = requests.post(m5stick_url, json=data_to_m5stick)
+#             # Send data to M5Stick
+#             response_to_m5stick = requests.post(m5stick_url, json=data_to_m5stick)
             
-            if response_to_m5stick.status_code == 200:
-                return JsonResponse({"status": "success", "message": "Data sent to M5Stick successfully"})
-            else:
-                return JsonResponse({"status": "error", "message": "Failed to send data to M5Stick"}, status=500)
+#             if response_to_m5stick.status_code == 200:
+#                 return JsonResponse({"status": "success", "message": "Data sent to M5Stick successfully"})
+#             else:
+#                 return JsonResponse({"status": "error", "message": "Failed to send data to M5Stick"}, status=500)
