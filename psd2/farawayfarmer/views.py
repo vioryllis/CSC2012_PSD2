eaa69fe74562319f309d1a8a88cab97f4c637a18
vioryllis <jedzvioletear@gmail.com>
2024-03-24 21:07:07 +0800

    removed unused function

--- psd2/farawayfarmer/views.py
+++ psd2/farawayfarmer/views.py
@@ -61,26 +61,6 @@ def logout_view(request):
     logout(request)
     return redirect('login')
 
-# @login_required 
-# def index(request):
-#     template = loader.get_template("dashboard.html")
-#     context = {}
-#     if not User.objects.all():
-#         u = User(name="user", email="user@mail.com", password="password", floor="floor")
-#         u.save()
-    
-#     if not Plant.objects.all():
-#         for i in range(10):
-#             p = Plant(name="Plant {}".format(i), 
-#                       floor="{}".format(random.choice(["1", "2", "3", "4", "5"])), 
-#                       public=True, 
-#                       auto_system=random.choice([True, False]), 
-#                       min_water_level=random.uniform(0, 100), 
-#                       amt_to_water=random.uniform(0, 100), 
-#                       user=User.objects.all()[0])
-#             p.save()
-#     return HttpResponse(template.render(context, request))
-
 @login_required 
 @csrf_exempt
 def add_plant(request):
