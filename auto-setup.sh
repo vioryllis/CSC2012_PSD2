9b0627a8bed4bfebeb563be809469f8ae191517d
Gabriel <40619564+gabriel-lau@users.noreply.github.com>
2024-03-24 12:34:48 +0800

    execute scripts update

--- auto-setup.sh
+++ auto-setup.sh
@@ -1,15 +1,6 @@
-# Stop all containers related to this docker-compose file
 docker-compose down
 
-# Remove all containers
-docker rm $(docker ps -a -q -f "name=farawayfarmer")
-docker rm $(docker ps -a -q -f "name=nearbyfarmer")
+docker rmi -f farawayfarmer
+docker rmi -f nearbyfarmer
 
-
-# Remove all images related to this project
-docker rmi $(docker images -q -f "reference=farawayfarmer")
-docker rmi $(docker images -q -f "reference=nearbyfarmer")
-
-
-# Run docker-compose
 docker-compose up -d
